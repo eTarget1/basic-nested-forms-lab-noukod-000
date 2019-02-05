efb93a47b8e3bd03bd446d18caed3e70526e0868
clifford <clifford@noukod.com>
2019-02-05 18:20:20 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,8 +10,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe = ingredients.build(name: "")
-    @recipe = ingredients.build(name: "")
+    @recipe = ingredients.build(name: "sugar")
+    @recipe = ingredients.build(name: "vanilla")
   end
 
   def create
